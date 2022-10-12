2b66458856f6854a8a6f9bb783fc0140dfbbae04
Rudra-Dhara <rudra.dhara1141@gmail.com>
2022-10-12 23:48:41 +0545

    First

---
+++ array.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    double a[10];
+    double b[10]={0,1};
+    for( int i=0; i<10;i++){
+        cout<<a[i]<<" "<<i i<< b[i]<< endl;
+    }
+    cout<<"ti"<<a[10]<<endl;
+    return 0;
+}
